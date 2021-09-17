94e0efa71a19b97533845ec0f9b98eb7735f4a32
Xiu <55036290+xius666@users.noreply.github.com>
2021-09-16 21:14:59 -0600

    Update proxy_client.py

--- proxy_client.py
+++ proxy_client.py
@@ -1,73 +1,33 @@
-#!/usr/bin/env python3
-import socket, sys
 
-#create a tcp socket
-def create_tcp_socket():
-    print('Creating socket')
+import socket
+
+
+BUFFER_SIZE = 1024
+HOST = 'localhost'
+PORT = 8001
+payload = f'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
+
+
+def connect(addr):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except (socket.error, msg):
-        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
-        sys.exit()
-    print('Socket created successfully')
-    return s
-
-#get host information
-def get_remote_ip(host):
-    print(f'Getting IP for {host}')
-    try:
-        remote_ip = socket.gethostbyname( host )
-    except socket.gaierror:
-        print ('Hostname could not be resolved. Exiting')
-        sys.exit()
-
-    print (f'Ip address of {host} is {remote_ip}')
-    return remote_ip
-
-#send data to server
-def send_data(serversocket, payload):
-    print("Sending payload")    
-    try:
-        serversocket.sendall(payload.encode())
-    except socket.error:
-        print ('Send failed')
-        sys.exit()
-    print("Payload sent successfully")
-
-def main():
-    try:
-        #define address info, payload, and buffer size
-        host = 'www.google.com'
-        payload = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
-        buffer_size = 4096
-
-        #make the socket, get the ip, and connect
-        s = create_tcp_socket()
-        proxy_host = 'localhost'
-
-        remote_ip = get_remote_ip(proxy_host)
-        port = 8001
-
-        s.connect((remote_ip , port))
-        print (f'Socket Connected to {host} on ip {remote_ip}')
-        
-        #send the data and shutdown
-        send_data(s, payload)
+        s.connect(addr)
+        s.sendall(payload.encode())
         s.shutdown(socket.SHUT_WR)
 
-        #continue accepting data until no more left
-        full_data = b""
-        while True:
-            data = s.recv(buffer_size)
-            if not data:
-                 break
-            full_data += data
+        full_data = s.recv(BUFFER_SIZE)
         print(full_data)
+
     except Exception as e:
         print(e)
     finally:
-        #always close at the end!
         s.close()
+
+
+def main():
+    connect((HOST, PORT))
+
+
 if __name__ == "__main__":
     main()
 
